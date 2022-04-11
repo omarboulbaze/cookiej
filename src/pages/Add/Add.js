052bd81c3bbf4b2b9542faee10ec94a624a41cec
KruserG <yokariig@gmail.com>
2022-04-11 19:50:55 -0400

    Adding calendar input to specify date of the cookie, it can be Today or an old date

--- src/pages/Add/Add.js
+++ src/pages/Add/Add.js
@@ -1,6 +1,19 @@
+import { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar } from "@fortawesome/free-regular-svg-icons";
 import Topbar from "../Components/Topbar/Topbar";
 import "./Add.css";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
 function Add(){
+
+    const date = new Date();
+    const defaultValue = date.toLocaleDateString('en-CA');
+
+    const inputRef = useRef(null);
+    const handleClick = () => {
+        inputRef.current.click();
+      };
+
 
     return(
         <>
@@ -13,6 +26,12 @@ function Add(){
                 <div className="input-group">
                     <textarea type="text" className="input" placeholder=" " /> 
                     <label className="placeholder">Description</label>
+                </div>
+                <div className="input-container">
+                    <FontAwesomeIcon icon={faCalendar} className="icon"/>
+                    <label className="label">Today</label>
+                    <input ref={inputRef} type="date" defaultValue={defaultValue}/>
+                    <FontAwesomeIcon icon={faPencil} className="icon edit" onClick={handleClick}/>
                 </div>
                
             </div>
