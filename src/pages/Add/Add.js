9a5fd1296a0132e60a0aff2afee98f563044c39a
KruserG <yokariig@gmail.com>
2022-04-20 17:58:00 -0400

    Image input is now at the top. A preview is showed when the user uploads an image.

--- src/pages/Add/Add.js
+++ src/pages/Add/Add.js
@@ -1,15 +1,18 @@
 
 //React components
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Topbar from "../Components/Topbar/Topbar";
 
 //Fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendar, faImage } from "@fortawesome/free-regular-svg-icons";
+import { faCalendar } from "@fortawesome/free-regular-svg-icons";
 import { faAngleDown, faAngleUp, faCamera, faCheckCircle, faPlus, faRankingStar, faTag } from "@fortawesome/free-solid-svg-icons";
 
 //CSS
 import "./Add.css";
+
+// Cookie Background image
+import cookieBg from "../../assets/cookiebg.jpg";
 
 // #region Time ago 
 const epochs = [
@@ -45,60 +48,99 @@ const timeAgo = (date) => {
 function Add(){
 
     const today = new Date().toLocaleDateString('en-CA');
-    const [date, setDate] = useState(today);
     
-    //tagsOpen
+    //are tags openned ?
     const [tagsOpen, setTagsOpen] = useState(false)
 
     //is the image uploaded ?
-    const [image, setImage] = useState(false)
+    const [imageUploaded, setImageUploaded] = useState(false)
     
+    // useState from Title, Description, Date, Rank, Tags, Image.
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [date, setDate] = useState(today);
+    const [rank, setRank] = useState("bronze");
+    const [image, setImage] = useState(null);
+    const [imgData, setImgData] = useState(null);
+
+
     function dateChecker(e){
         if(e.target.value>today){
             alert("You can't add a cookie in the future.");
             setDate(today);
-        }else{
+        }else if(!e.target.value){
+            setDate(today);
+        }
+        else{
             setDate(e.target.value);
         }
         
     }
 
+    //Creating the reference of the HTML input tag.
+    const imageInputFile = useRef(null);
+    function divClick(){
+        imageInputFile.current.click();
+    }
+    //When user uploads an image
+    function onImageUpload(e){
+        setImageUploaded(true);
+        if (e.target.files[0]) {
+            setImage(e.target.files[0]);
+            const reader = new FileReader(); //Reading the image from user input
+            reader.addEventListener("load", () => {
+              setImgData(reader.result);
+            });
+            reader.readAsDataURL(e.target.files[0]);
+          }
+    }
+
     return(
         <>
             <Topbar text="New Cookie"/>
-            <div className="add_form" style={{maxWidth:"640px", margin:"auto"}}>
+            <form className="add_form" style={{maxWidth:"640px", margin:"auto"}}>
 
+                {/* Image input */}
+                <div className="img-container" onClick={()=> divClick()}>
+                        <img src={imageUploaded ? imgData : cookieBg} alt="Cookie Background"/>
+                        <label>{imageUploaded ? <FontAwesomeIcon icon={faCheckCircle} className="icon" style={{color:"#ffae44"}}/> : <FontAwesomeIcon icon={faCamera} className="icon"/> }</label>
+                        {/* Input is hidden. Only the label is visible which is linked to the input tag. */}
+                        <input ref={imageInputFile} capture="environment" accept="image/*" type="file" style={{display:"none"}} onChange={ e => onImageUpload(e)}/>
+                        <label className="label" style={{padding:"0.5rem"}}>{imageUploaded ? "Image successfully added!" : "Add Image"}</label>
+                </div>
+                {/* Title input */}
                 <div className="input-group">
-                    <input type="text" className="input" placeholder=" " style={{"borderBottom":"0px"}}/>
+                    <input type="text" className="input" placeholder=" " style={{"borderBottom":"0px"}} required value={title} onChange={e =>setTitle(e.target.value)}/>
                     <label className="placeholder">Title</label>
                 </div>
-
+                {/* Description input */}
                 <div className="input-group">
-                    <textarea type="text" className="input" placeholder=" " /> 
+                    <textarea type="text" className="input" placeholder=" " value={description} onChange={e =>setDescription(e.target.value)}/> 
                     <label className="placeholder">Description</label>
                 </div>
-
+                {/* Date input */}
                 <div className="input-container">
                     <div>
                         <FontAwesomeIcon icon={faCalendar} className="icon"/>
                         <label className="label">{date===today ? "Today" : timeAgo(date)}</label>
                     </div>
-                    <input type="date" value={date} max={today} onChange={(e)=> dateChecker(e)} required style={{fontSize:"1rem"}}/>
+                    <input required type="date" value={date} max={today} onChange={ e=> dateChecker(e)} style={{fontSize:"1rem"}}/>
                 </div>
-
+                {/* Rank input */}
                 <div className="input-container">
                     <div>
                         <FontAwesomeIcon icon={faRankingStar} className="icon"/>
                         <label className="label">Rank</label>
                     </div>
-                    <select className="select" style={{fontSize:"1rem"}}>
-                        <option className="bronze">🟤 Bronze</option>
-                        <option className="silver">⚪️ Silver</option>
-                        <option className="gold">🟡 Gold</option>
-                        <option className="platinum">🔵 Platinum</option>
-                        <option className="diamond">🟣 Diamond</option>
+                    <select className="select" style={{fontSize:"1rem"}} value={rank} onChange={e => setRank(e.target.value)}>
+                        <option value="bronze" className="bronze">🟤 Bronze</option>
+                        <option value="silver" className="silver">⚪️ Silver</option>
+                        <option value="gold" className="gold">🟡 Gold</option>
+                        <option value="platinum" className="platinum">🔵 Platinum</option>
+                        <option value="diamond" className="diamond">🟣 Diamond</option>
                     </select>
                 </div>
+                {/* Tags input */}
                 <div className="tags-container">
                     <div className="lineOne" onClick={ tagsOpen ? ()=>setTagsOpen(false) : ()=>setTagsOpen(true)}>
                         <div>
@@ -121,19 +163,12 @@ function Add(){
                     : null
                     }
                 </div>
-                <div className="input-container">
-                    <div>
-                        <FontAwesomeIcon icon={faImage} className="icon"/>
-                        <label className="label">Image</label>
-                    </div>
-                    <label htmlFor="file">{image ? <FontAwesomeIcon icon={faCheckCircle} className="icon" style={{color:"#ffae44"}}/> : <FontAwesomeIcon icon={faCamera} className="icon"/> }</label>
-                    <input id="file" capture="environment" accept="image/*" type="file" style={{display:"none"}} onChange={()=> setImage(true)}/>
-                </div>
+                
                 <div className="button-container">
-                    <button>Add Cookie</button>
+                    <button type="submit">Add Cookie</button>
                 </div>
                
-            </div>
+            </form>
         
         </>
     )
