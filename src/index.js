fa1f5bb49c13f56049b689b52e67d94824e3ae8c
KruserG <yokariig@gmail.com>
2022-06-06 20:36:03 -0400

    Redux Toolkit setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
+// Redux
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 
-ReactDOM.render( <Root/> , document.getElementById('root') );
+ReactDOM.render( <Provider store={store}> <Root/> </Provider> , document.getElementById('root') );
 
