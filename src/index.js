9ac7b62bc19707c966e8584d0e57003484596b1e
Omar Boulbaze <omar.boulbaze@ibm.com>
2022-06-11 06:23:03 -0400

    Fixing npm modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
-// Redux
-import { store } from './app/store'
-import { Provider } from 'react-redux'
 
 
-ReactDOM.render( <Provider store={store}> <Root/> </Provider> , document.getElementById('root') );
+
+ReactDOM.render( <Root/> , document.getElementById('root') );
 
