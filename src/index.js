7a3c35586950755ae82b6425ec2add05e3530090
omarboulbaze <yokariig@gmail.com>
2022-06-18 11:30:07 -0400

    Deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 
-// Redux test
+// Redux
 import { Provider } from 'react-redux';
 import store from './store/store';
 
