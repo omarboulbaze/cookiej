6ede8a1c55421925ca27eba1eba75c23a47190d5
omarboulbaze <yokariig@gmail.com>
2022-06-18 07:35:46 -0400

    Testing CI/CD

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 
-// Redux
+// Redux test
 import { Provider } from 'react-redux';
 import store from './store/store';
 
